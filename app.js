89ebe9deee7025495c258f090534681ccdc730c0
Edoardo De Piccoli <edoardodepiccoli@Edoardos-MacBook-Air.local>
2023-09-30 15:52:59 +0200

    YES IT WORKS

--- app.js
+++ app.js
@@ -1,120 +1,125 @@
-let firstOperand = 0
-let secondOperand = 0
-let currentOperation = null
-let shouldClearDisplay = false
-let done = false
+let firstOperand = null;
+let secondOperand = null;
+let currentOperation = null;
+let shouldClearDisplay = false;
 
-const numbers = document.querySelectorAll(".number")
-const operations = document.querySelectorAll(".operation")
+const numbers = document.querySelectorAll(".number");
+const operations = document.querySelectorAll(".operation");
+const topd = document.querySelector(".display-top");
+const bottomd = document.querySelector(".display-bottom");
+const equalsButton = document.querySelector(".equals");
+const clearButton = document.querySelector(".clear");
+const deleteButton = document.querySelector(".delete");
+const dotButton = document.querySelector(".dot");
 
-const lastOperationDisplay = document.querySelector(".display-top")
-const currentOperationDisplay = document.querySelector(".display-bottom")
-
-const deleteButton = document.querySelector(".delete")
-const clearButton = document.querySelector(".clear")
-
-const dotButton = document.querySelector(".dot")
-
-for(let number of numbers){ number.addEventListener("click", ()=>{appendNumber(number.textContent)}) }
-for(let op of operations){ op.addEventListener("click", ()=>{ setOperation(op.textContent) }) }
-deleteButton.addEventListener("click", deleteNumber)
-clearButton.addEventListener("click", clearAll)
-dotButton.addEventListener("click", addDot)
-
-function add(a,b){return a+b}
-function subtract(a,b){return a-b}
-function multiply(a,b){return a*b}
-function divide(a,b){return a/b}
-
-function operate(operator, a, b){
-    shouldClearDisplay = true
-    done = true
-    switch(operator){
-        case "+": return add(a,b)
-        case "-": return subtract(a,b)
-        case "*": return multiply(a,b)
-        case "/": return divide(a,b)
-        default: null
-    }
+function add(a, b) {
+  return a + b;
+}
+function subtract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  return a / b;
 }
 
-function resetDisplay(){ currentOperationDisplay.textContent = "" }
-function resetAllDisplays() { lastOperationDisplay.textContent = "" }
-
-function clearAll(){
-    firstOperand, secondOperand = 0
-    currentOperation = null
-    shouldClearDisplay = false
-    lastOperationDisplay.textContent = ""
-    resetDisplay()
+function operate(operator, a, b) {
+  shouldClearDisplay = false;
+  currentOperation = null;
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "*":
+      return multiply(a, b);
+    case "/":
+      return divide(a, b);
+    default:
+      null;
+  }
 }
 
-function appendNumber(text){
-    if(shouldClearDisplay) resetDisplay()
-    if(currentOperationDisplay.textContent==="0") {
-        currentOperationDisplay.textContent = text
-        return
-    }
-    currentOperationDisplay.textContent += text
-    shouldClearDisplay = false
+function clearDisplay() {
+  bottomd.textContent = "";
+  shouldClearDisplay = false;
 }
 
-function deleteNumber(){
-    if(currentOperationDisplay.textContent==="0" || currentOperationDisplay.textContent==="") return
-
-    let lastIndex = currentOperationDisplay.textContent.length-1
-    let lastIndexLastOp = lastOperationDisplay.textContent.length-1
-    if(lastOperationDisplay.textContent[lastIndexLastOp]===currentOperationDisplay.textContent[lastIndex]) {
-        lastOperationDisplay.textContent = lastOperationDisplay.textContent.slice(0,-1)
-    }
-
-    currentOperationDisplay.textContent = currentOperationDisplay.textContent.slice(0,-1)
+function clearAll() {
+  topd.textContent = "";
+  bottomd.textContent = "";
+  firstOperand = null;
+  secondOperand = null;
+  currentOperation = null;
+  shouldClearDisplay = false;
 }
 
-function addDot(){
-    text = currentOperationDisplay.textContent
-    lastIndex = text.length - 1
-    if(currentOperationDisplay.textContent===""){
-        currentOperationDisplay.textContent = "0"
-        return
-    }
-    if(text[lastIndex]===".") return
-    if(text.includes(".")) return
-    currentOperationDisplay.textContent += "."
+for (let button of numbers) {
+  button.addEventListener("click", () => appendNumber(button.textContent));
+}
+for (let button of operations) {
+  button.addEventListener("click", () => setOperation(button.textContent));
 }
 
-function setOperation(text){
-    console.log(currentOperation)
-    console.log(text)
-    if(currentOperation!=null && shouldClearDisplay===true) {
-        console.log("change operator")
-        currentOperationDisplay.textContent = text
-        lastOperationDisplay.textContent += text
-        currentOperation = text
-        shouldClearDisplay = true
-    }
-    else if(currentOperation!=null && shouldClearDisplay===false) {
-        console.log("should compute")
-        secondOperand = currentOperationDisplay.textContent
-        lastOperationDisplay.textContent += `${secondOperand}=`
-        let result = operate(currentOperation, Number(firstOperand), Number(secondOperand))
-        currentOperationDisplay.textContent = result
-        lastOperationDisplay.textContent += result
-        if(text!="=") lastOperationDisplay.textContent += text
-        firstOperand = Number(result)
-        shouldClearDisplay = true
-        currentOperation = text
-        return
-    }
-    else{
-        console.log("normal operation")
-        firstOperand = Number(currentOperationDisplay.textContent)
-        lastOperationDisplay.textContent = firstOperand + text
-        // secondOperand = currentOperationDisplay.textContent
-        // lastOperationDisplay.textContent += `${secondOperand}=`
-        currentOperationDisplay.textContent = text
-        currentOperation = text
-        shouldClearDisplay = true
-    }
+equalsButton.addEventListener("click", evaluate);
+
+clearButton.addEventListener("click", clearAll);
+
+dotButton.addEventListener("click", addDot);
+
+deleteButton.addEventListener("click", deleteNumber);
+
+function appendNumber(text) {
+  if (shouldClearDisplay || bottomd.textContent === "0") clearDisplay();
+  bottomd.textContent += text;
 }
 
+function addDot() {
+  if (bottomd.textContent.includes(".")) return;
+  if (bottomd.textContent === "0") {
+    bottomd.textContent += ".";
+  } else if (bottomd.textContent === "") {
+    bottomd.textContent = "0.";
+  } else {
+    bottomd.textContent += ".";
+  }
+}
+
+function deleteNumber() {
+  if (bottomd.textContent === "" || bottomd.textContent === "0") return;
+  bottomd.textContent = bottomd.textContent.slice(0, -1);
+}
+
+// come faccio a sapere se ho già cliccato l'opreazione e voglio solamente cambiarla????
+// verifica se non è uguale a null ma, allo stesso tempo, se shoulcleardisplay è falso
+function setOperation(text) {
+  if (currentOperation != null && !shouldClearDisplay) evaluate();
+
+  console.log("operazione normale");
+  firstOperand = bottomd.textContent;
+  currentOperation = text;
+  topd.textContent = `${firstOperand} ${currentOperation}`;
+
+  shouldClearDisplay = true;
+}
+
+function evaluate() {
+  if (firstOperand === null || currentOperation === null) {
+    console.log("not possible");
+    return;
+  }
+  secondOperand = bottomd.textContent;
+  topd.textContent += `${secondOperand} =`;
+
+  let result = operate(
+    currentOperation,
+    Number(firstOperand),
+    Number(secondOperand)
+  );
+
+  bottomd.textContent = result;
+  firstOperand = result;
+  currentOperation = null;
+}
