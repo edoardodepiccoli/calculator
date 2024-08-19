1e964f398d0fdbd220ed79bd8b4a23d1e62e4ea2
Edoardo De Piccoli <103844232+edoardodepiccoli@users.noreply.github.com>
2024-08-19 19:46:00 +0200

    delete all the logic, start from scratch again

--- app.js
+++ app.js
@@ -1,50 +1,5 @@
-let firstOperand = null;
-let secondOperand = null;
-let currentOperator = null;
-
-let toCompute = false;
-
-function log() {
-  console.log(
-    `first = ${firstOperand}, second = ${secondOperand}, current = ${currentOperator}`
-  );
-}
-
-function setOperand(value) {
-  if (!toCompute) {
-    if (firstOperand) {
-      firstOperand = String(firstOperand) + String(value);
-    } else {
-      firstOperand = String(value);
-    }
-  } else {
-    if (secondOperand) {
-      secondOperand = String(secondOperand) + String(value);
-    } else {
-      secondOperand = String(value);
-    }
-  }
-
-  log();
-}
-
-function setOperator(symbol) {}
-
-function operate() {}
-
 // dom references
 const buttonsContainer = document.querySelector(".buttons-container");
-
-const resetButton = document.querySelector("#reset-button");
-const changeSignButton = document.querySelector("#change-sign-button");
-const percentButton = document.querySelector("#percent-button");
-const commaButton = document.querySelector("#comma-button");
-
-const divideButton = document.querySelector("#divide-button");
-const multiplyButton = document.querySelector("#multiply-button");
-const subtractButton = document.querySelector("#subtract-button");
-const addButton = document.querySelector("#add-button");
-const computeButton = document.querySelector("#compute-button");
 
 // event listeners
 buttonsContainer.addEventListener("click", (e) => {
@@ -53,7 +8,7 @@ buttonsContainer.addEventListener("click", (e) => {
     setOperand(e.target.innerText);
   } else if (e.target.matches(".operator-button")) {
     console.log("clicked on operator button");
-    setOperator(e.target.innerText);
+    setOperator(e.target.dataset.value);
   } else if (e.target.matches(".utility-button")) {
     console.log("clicked on an utility button");
   }
