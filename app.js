40b9138c9af38ae34b22e50e1499d84d778624d6
Edoardo De Piccoli <103844232+edoardodepiccoli@users.noreply.github.com>
2024-08-19 21:43:07 +0200

    create operate function

--- app.js
+++ app.js
@@ -1,10 +1,41 @@
+// app logic
+let first = null;
+let second = null;
+let operator = null;
+
+let toOperate = false;
+let startAgain = true;
+
+function log() {
+  console.log(
+    `first = ${first}, second = ${second}, operator = ${operator}\ntoOperate = ${toOperate}, startAgain = ${startAgain}`
+  );
+}
+
+function operate() {
+  if (operator === "add") {
+    first = Number(first) + Number(second);
+  } else if (operator === "subtract") {
+    first = Number(first) - Number(second);
+  } else if (operator === "multiply") {
+    first = Number(first) * Number(second);
+  } else if (operator === "divide") {
+    first = Number(first) / Number(second);
+  }
+}
+
 // dom references
 const buttonsContainer = document.querySelector(".buttons-container");
 
 // event listeners
 buttonsContainer.addEventListener("click", (e) => {
   if (e.target.matches(".operand-button")) {
+    console.log("clicked operand button");
   } else if (e.target.matches(".operator-button")) {
+    console.log("clicked operator button");
+  } else if (e.target.matches(".compute-button")) {
+    console.log("clicked compute button");
   } else if (e.target.matches(".utility-button")) {
+    console.log("clicked utility button");
   }
 });
