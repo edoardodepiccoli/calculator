fe1b35210f95def9d9e570429440841516b444e9
Edoardo De Piccoli <103844232+edoardodepiccoli@users.noreply.github.com>
2024-08-19 19:46:39 +0200

    delete pseudocode from notex.txt

--- app.js
+++ app.js
@@ -4,12 +4,7 @@ const buttonsContainer = document.querySelector(".buttons-container");
 // event listeners
 buttonsContainer.addEventListener("click", (e) => {
   if (e.target.matches(".operand-button")) {
-    console.log("clicked on operand button");
-    setOperand(e.target.innerText);
   } else if (e.target.matches(".operator-button")) {
-    console.log("clicked on operator button");
-    setOperator(e.target.dataset.value);
   } else if (e.target.matches(".utility-button")) {
-    console.log("clicked on an utility button");
   }
 });
